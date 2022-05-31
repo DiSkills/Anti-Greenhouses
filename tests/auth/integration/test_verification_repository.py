ba79a90db078e32a88d9440cc9a5c7dc915fc68d
_Anti_ <anti.python.fastapi@gmail.com>
2022-05-31 14:45:32 +0300

    Method "Remove" in Verification repository

--- tests/auth/integration/test_verification_repository.py
+++ tests/auth/integration/test_verification_repository.py
@@ -20,15 +20,15 @@ def test_repository_can_save_a_verification(sqlite_session):
 
 
 def test_repository_can_retrieve_a_verification(sqlite_session):
-    _uuid1 = f'{uuid.uuid4()}'
-    _uuid2 = f'{uuid.uuid4()}'
+    uuid1 = f'{uuid.uuid4()}'
+    uuid2 = f'{uuid.uuid4()}'
 
     sqlite_session.execute(
         'INSERT INTO verifications (email, uuid) VALUES (:email1, :uuid1), (:email2, :uuid2)',
-        {'email1': 'user@example.com', 'uuid1': _uuid1, 'email2': 'python@example.com', 'uuid2': _uuid2},
+        {'email1': 'user@example.com', 'uuid1': uuid1, 'email2': 'python@example.com', 'uuid2': uuid2},
     )
 
-    expected = model.Verification(email='python@example.com', uuid=_uuid2)
+    expected = model.Verification(email='python@example.com', uuid=uuid2)
     repo = repository.VerificationRepository(session=sqlite_session)
 
     # Get by email
@@ -36,17 +36,17 @@ def test_repository_can_retrieve_a_verification(sqlite_session):
     assert expected == retrieved
 
     # Get by uuid
-    retrieved = repo.get(uuid=_uuid1)
-    assert retrieved == model.Verification(email='user@example.com', uuid=_uuid1)
+    retrieved = repo.get(uuid=uuid1)
+    assert retrieved == model.Verification(email='user@example.com', uuid=uuid1)
 
 
 def test_repository_can_not_retrieve_a_verification(sqlite_session):
-    _uuid1 = f'{uuid.uuid4()}'
-    _uuid2 = f'{uuid.uuid4()}'
+    uuid1 = f'{uuid.uuid4()}'
+    uuid2 = f'{uuid.uuid4()}'
 
     sqlite_session.execute(
         'INSERT INTO verifications (email, uuid) VALUES (:email1, :uuid1), (:email2, :uuid2)',
-        {'email1': 'user@example.com', 'uuid1': _uuid1, 'email2': 'python@example.com', 'uuid2': _uuid2},
+        {'email1': 'user@example.com', 'uuid1': uuid1, 'email2': 'python@example.com', 'uuid2': uuid2},
     )
 
     repo = repository.VerificationRepository(session=sqlite_session)
@@ -56,40 +56,40 @@ def test_repository_can_not_retrieve_a_verification(sqlite_session):
 
 
 def test_repository_can_remove_verification_by_email(sqlite_session):
-    _uuid1 = f'{uuid.uuid4()}'
-    _uuid2 = f'{uuid.uuid4()}'
+    uuid1 = f'{uuid.uuid4()}'
+    uuid2 = f'{uuid.uuid4()}'
 
     sqlite_session.execute(
         'INSERT INTO verifications (email, uuid) VALUES (:email1, :uuid1), (:email2, :uuid2)',
-        {'email1': 'user@example.com', 'uuid1': _uuid1, 'email2': 'python@example.com', 'uuid2': _uuid2},
+        {'email1': 'user@example.com', 'uuid1': uuid1, 'email2': 'python@example.com', 'uuid2': uuid2},
     )
 
     rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('user@example.com', _uuid1), ('python@example.com', _uuid2))
+    assert rows == (('user@example.com', uuid1), ('python@example.com', uuid2))
 
     repo = repository.VerificationRepository(session=sqlite_session)
     repo.remove(email='user@example.com')
     sqlite_session.commit()
 
     rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('python@example.com', _uuid2),)
+    assert rows == (('python@example.com', uuid2),)
 
 
 def test_repository_can_remove_verification_by_uuid(sqlite_session):
-    _uuid1 = f'{uuid.uuid4()}'
-    _uuid2 = f'{uuid.uuid4()}'
+    uuid1 = f'{uuid.uuid4()}'
+    uuid2 = f'{uuid.uuid4()}'
 
     sqlite_session.execute(
         'INSERT INTO verifications (email, uuid) VALUES (:email1, :uuid1), (:email2, :uuid2)',
-        {'email1': 'user@example.com', 'uuid1': _uuid1, 'email2': 'python@example.com', 'uuid2': _uuid2},
+        {'email1': 'user@example.com', 'uuid1': uuid1, 'email2': 'python@example.com', 'uuid2': uuid2},
     )
 
     rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('user@example.com', _uuid1), ('python@example.com', _uuid2))
+    assert rows == (('user@example.com', uuid1), ('python@example.com', uuid2))
 
     repo = repository.VerificationRepository(session=sqlite_session)
-    repo.remove(uuid=_uuid1)
+    repo.remove(uuid=uuid1)
     sqlite_session.commit()
     
     rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('python@example.com', _uuid2),)
+    assert rows == (('python@example.com', uuid2),)
