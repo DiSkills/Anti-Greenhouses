c3653a775b90b7a84b014495f0a134db57c43e1a
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-02 18:07:19 +0300

    FastAPI init

--- config.py
+++ config.py
@@ -1,4 +1,20 @@
-# TODO config
+import os
+from dataclasses import dataclass
+
 import sqlalchemy
 
 metadata = sqlalchemy.MetaData()
+
+
+@dataclass
+class AppConfig:
+    title: str
+    version: str
+    description: str
+
+
+def get_app_settings() -> AppConfig:
+    title = os.environ.get('TITLE', 'Anti-Greenhouses')
+    version = os.environ.get('VERSION', '0.1.0')
+    description = os.environ.get('DESCRIPTION', 'Anti-Greenhouses by _Anti_')
+    return AppConfig(title=title, version=version, description=description)
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# TODO FastAPI initialization
+from fastapi import FastAPI
+
+import config
+
+app_config = config.get_app_settings()
+
+app = FastAPI(
+    title=app_config.title,
+    version=app_config.version,
+    description=app_config.description,
+)
