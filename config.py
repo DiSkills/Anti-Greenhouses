3726b5e311debd076d4fb9825925f020cf8fb76d
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-02 18:15:59 +0300

    Config (get_db_uri, get_host_url)

--- config.py
+++ config.py
@@ -18,3 +18,17 @@ def get_app_settings() -> AppConfig:
     version = os.environ.get('VERSION', '0.1.0')
     description = os.environ.get('DESCRIPTION', 'Anti-Greenhouses by _Anti_')
     return AppConfig(title=title, version=version, description=description)
+
+
+def get_db_uri() -> str:
+    host = os.environ.get('DB_HOST', 'localhost')
+    port = os.environ.get('DB_PORT', '5432')
+    db_name = os.environ.get('DB_NAME', 'DB_NAME')
+    db_user = os.environ.get('DB_USER', 'DB_USER')
+    db_password = os.environ.get('DB_PASSWORD', 'DB_PASSWORD')
+    return f'postgresql://{db_user}:{db_password}@{host}:{port}/{db_name}'
+
+
+def get_host_url() -> str:
+    url = os.environ.get('HOST', 'http://localhost:8000')
+    return url
