7cc52d010cab46305504451c7dff4cc9c55273da
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-02 11:54:07 +0300

    FastAPI init

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-# TODO config
+import os
+import typing
+
 import sqlalchemy
 
 metadata = sqlalchemy.MetaData()
+
+
+def get_app_config() -> dict[typing.Literal['TITLE', 'VERSION', 'DESCRIPTION'], str]:
+    TITLE = os.environ.get('TITLE', 'TITLE')
+    VERSION = os.environ.get('VERSION', '0.1.0')
+    DESCRIPTION = os.environ.get('DESCRIPTION', 'DESCRIPTION')
+    return {'TITLE': TITLE, 'VERSION': VERSION, 'DESCRIPTION': DESCRIPTION}
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# TODO FastAPI initialization
+from fastapi import FastAPI
+
+import config
+
+app = FastAPI(
+    title=config.get_app_config()['TITLE'],
+    version=config.get_app_config()['VERSION'],
+    description=config.get_app_config()['DESCRIPTION'],
+)
