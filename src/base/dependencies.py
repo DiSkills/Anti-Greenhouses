c814c6ceee0cb9d309087f261386daef573b0835
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-02 12:45:42 +0300

    Database + FastAPI init

--- src/base/dependencies.py
+++ src/base/dependencies.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
 import config
 
 
-def get_db() -> Session:
+def get_db():
     get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
     with get_session() as db:
         yield db
