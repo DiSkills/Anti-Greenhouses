327523a747e6086d45218ed968a50441102e4b0c
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-02 13:21:08 +0300

    Added postgres fixtures in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+import time
+
 import pytest
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker, clear_mappers, Session
 
+import config
 from config import metadata
 from src.auth.adapters.orm import start_mappers as auth_start_mappers
 
@@ -21,3 +26,33 @@ def in_memory_db():
 @pytest.fixture()
 def sqlite_session(in_memory_db) -> Session:
     return in_memory_db()
+
+
+def wait_for_postgres_to_come_up(*, engine: Engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+    pytest.fail('Postgres never came up')
+
+
+@pytest.fixture()
+def postgres_db():
+    engine = create_engine(config.get_postgres_uri())
+    wait_for_postgres_to_come_up(engine=engine)
+
+    with engine.begin() as engine:
+        metadata.create_all(engine)
+
+        auth_start_mappers()
+        yield sessionmaker(bind=engine)
+        clear_mappers()
+
+        metadata.drop_all(engine)
+
+
+@pytest.fixture()
+def postgres_session(postgres_db) -> Session:
+    return postgres_db()
