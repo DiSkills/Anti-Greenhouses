4b0f2956edf62bd0b1e9fdb6c5b6e597fb6bfb8c
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-09 13:07:28 +0300

    Added unique in verifications.uuid

--- src/auth/adapters/orm.py
+++ src/auth/adapters/orm.py
@@ -8,7 +8,7 @@ verifications = sqlalchemy.Table(
     'verifications',
     metadata,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True, nullable=False),
-    sqlalchemy.Column('uuid', sqlalchemy.String, nullable=False),
+    sqlalchemy.Column('uuid', sqlalchemy.String, unique=True, nullable=False),
     sqlalchemy.Column('email', sqlalchemy.String, unique=True, nullable=False),
 )
 
