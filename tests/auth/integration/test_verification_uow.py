7af083238e646b0e8c33e58775aeaa5311ef3254
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-20 16:42:41 +0300

    Added UoW

---
+++ tests/auth/integration/test_verification_uow.py
@@ -0,0 +1,48 @@
+import uuid
+
+import pytest
+
+from src.auth.domain import model
+from src.auth.services.uow.verifications import VerificationUnitOfWork
+
+
+def test_uow_can_save_a_verification(in_memory_db):
+    _uuid = f'{uuid.uuid4()}'
+
+    uow = VerificationUnitOfWork(session_factory=in_memory_db)
+    with uow:
+        verification = model.Verification(email='user@example.com', uuid=_uuid)
+        uow.verifications.add(verification=verification)
+        uow.commit()
+
+    with in_memory_db() as sqlite_session:
+        rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
+        assert rows == (('user@example.com', _uuid),)
+
+
+def test_rolls_back_uncommitted_work_by_default(in_memory_db):
+    _uuid = f'{uuid.uuid4()}'
+
+    uow = VerificationUnitOfWork(session_factory=in_memory_db)
+    with uow:
+        verification = model.Verification(email='user@example.com', uuid=_uuid)
+        uow.verifications.add(verification=verification)
+
+    with in_memory_db() as sqlite_session:
+        rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
+        assert rows == ()
+
+
+def test_rolls_back_on_error(in_memory_db):
+    _uuid = f'{uuid.uuid4()}'
+
+    uow = VerificationUnitOfWork(session_factory=in_memory_db)
+    with pytest.raises(Exception):
+        with uow:
+            verification = model.Verification(email='user@example.com', uuid=_uuid)
+            uow.verifications.add(verification=verification)
+            raise Exception()
+
+    with in_memory_db() as sqlite_session:
+        rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
+        assert rows == ()
