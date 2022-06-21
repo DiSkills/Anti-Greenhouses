23966b377f85c90caf0e42c30fe83301b50b6e47
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-21 20:08:54 +0300

    Changed FakeVerificationUnitOfWork

--- tests/auth/fake_uow.py
+++ tests/auth/fake_uow.py
@@ -4,8 +4,6 @@ from tests.auth.fake_repositories import FakeSession, FakeVerificationRepository
 class FakeVerificationUnitOfWork:
     """ Fake verification Unit of Work """
 
-    verifications: FakeVerificationRepository
-
     def __init__(self) -> None:
         self.verifications = FakeVerificationRepository(session=FakeSession(), verifications=[])
         self.committed = False
