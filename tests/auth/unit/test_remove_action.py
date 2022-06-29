6df1253180d34fff586fc15b9c1c45d2c9a9f101
_Anti_ <anti.python.fastapi@gmail.com>
2022-06-29 11:16:06 +0300

    Added add_action & remove_action in domain model

--- tests/auth/unit/test_remove_action.py
+++ tests/auth/unit/test_remove_action.py
@@ -5,27 +5,32 @@ import config
 from src.auth.domain import model
 
 
-def test_user_can_add_action():
+def test_user_can_remove_action():
     user = model.User(username='test', email='user@example.com', password='password', otp_secret='secret')
     action = model.UserAction(
-        uuid=f'{uuid.uuid4()}', type=config.UserActionType.registered, created_at=datetime.utcnow(),
+        uuid=f'{uuid.uuid4()}',
+        type=config.UserActionType.registered,
+        created_at=datetime.utcnow(),
+        ip_address='0.0.0.0',
     )
     action2 = model.UserAction(
         uuid=f'{uuid.uuid4()}', type=config.UserActionType.registered, created_at=datetime.utcnow(),
     )
 
-    assert user.count_actions == 0
-    assert user.actions == []
-
     model.add_action(action=action, user=user)
-    assert user.count_actions == 1
-    assert user.actions == [action]
-
     model.add_action(action=action2, user=user)
     assert user.count_actions == 2
     assert sorted(user.actions, key=lambda _action: _action.created_at) == [action, action2]
 
-    # Add an existing one
-    model.add_action(action=action, user=user)
-    assert user.count_actions == 2
-    assert sorted(user.actions, key=lambda _action: _action.created_at) == [action, action2]
+    model.remove_action(action=action, user=user)
+    assert user.count_actions == 1
+    assert user.actions == [action2]
+
+    # Delete non-existent
+    model.remove_action(action=action, user=user)
+    assert user.count_actions == 1
+    assert user.actions == [action2]
+
+    model.remove_action(action=action2, user=user)
+    assert user.count_actions == 0
+    assert user.actions == []
