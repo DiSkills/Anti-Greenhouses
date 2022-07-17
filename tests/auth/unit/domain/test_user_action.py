7f0d4e3137fa0145b809554d8e2d89f94053aeb8
_Anti_ <anti.python.fastapi@gmail.com>
2022-07-17 21:50:23 +0300

    Refactoring test_user_action.py, conftest.py

--- tests/auth/unit/domain/test_user_action.py
+++ tests/auth/unit/domain/test_user_action.py
@@ -1,29 +1,30 @@
-import uuid
 from datetime import datetime
+from uuid import uuid4
 
 import config
 from src.auth.domain import model
+from tests.conftest import TestData
 
 
 def test_user_actions_are_equal_by_uuid():
-    _uuid = f'{uuid.uuid4()}'
+    uuid = f'{uuid4()}'
     first_user_action = model.UserAction(
-        uuid=_uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(), ip_address='0.0.0.0',
+        uuid=uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(), ip_address=TestData.ip_address,
     )
     second_user_action = model.UserAction(
-        uuid=_uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(),
+        uuid=uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(),
     )
 
     assert first_user_action == second_user_action
 
 
 def test_user_actions_are_not_equal_by_uuid():
-    _uuid = f'{uuid.uuid4()}'
+    uuid = f'{uuid4()}'
     first_user_action = model.UserAction(
-        uuid=_uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(), ip_address='0.0.0.0',
+        uuid=uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(), ip_address=TestData.ip_address,
     )
     second_user_action = model.UserAction(
-        uuid=f'{uuid.uuid4()}', type=config.UserActionType.registered, created_at=datetime.utcnow(),
+        uuid=f'{uuid4()}', type=config.UserActionType.registered, created_at=datetime.utcnow(),
     )
 
     assert (first_user_action == second_user_action) is False
@@ -34,9 +35,9 @@ def test_user_action_is_not_equal_another_object():
     class AnotherObject:
         pass
 
-    _uuid = f'{uuid.uuid4()}'
+    uuid = f'{uuid4()}'
     user_action = model.UserAction(
-        uuid=_uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(), ip_address='0.0.0.0',
+        uuid=uuid, type=config.UserActionType.registered, created_at=datetime.utcnow(), ip_address=TestData.ip_address,
     )
     another_object = AnotherObject()
 
@@ -45,10 +46,10 @@ def test_user_action_is_not_equal_another_object():
 
 def test_user_action_the_represent_method():
     user_action = model.UserAction(
-        uuid=f'{uuid.uuid4()}',
+        uuid=f'{uuid4()}',
         type=config.UserActionType.registered,
         created_at=datetime.utcnow(),
-        ip_address='0.0.0.0',
+        ip_address=TestData.ip_address,
     )
 
     assert user_action.__repr__() == f'<UserAction {user_action.uuid}>'
