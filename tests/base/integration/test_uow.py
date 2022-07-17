59dae1c89f2f169515b4f9e1946adbd5ab27a469
_Anti_ <anti.python.fastapi@gmail.com>
2022-07-17 20:33:38 +0300

    Refactoring /tests/base/

--- tests/base/integration/test_uow.py
+++ tests/base/integration/test_uow.py
@@ -7,25 +7,25 @@ from src.base.uow import UnitOfWork
 
 
 def test_uow_can_save_a_verification(in_memory_db):
-    _uuid = f'{uuid4()}'
+    uuid = f'{uuid4()}'
 
     uow = UnitOfWork(session_factory=in_memory_db)
     with uow:
-        verification = model.Verification(email='user@example.com', uuid=_uuid)
+        verification = model.Verification(email='user@example.com', uuid=uuid)
         uow.verifications.add(verification=verification)
         uow.commit()
 
     with in_memory_db() as sqlite_session:
         rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
-        assert rows == (('user@example.com', _uuid),)
+        assert rows == (('user@example.com', uuid),)
 
 
 def test_rolls_back_uncommitted_work_by_default(in_memory_db):
-    _uuid = f'{uuid4()}'
+    uuid = f'{uuid4()}'
 
     uow = UnitOfWork(session_factory=in_memory_db)
     with uow:
-        verification = model.Verification(email='user@example.com', uuid=_uuid)
+        verification = model.Verification(email='user@example.com', uuid=uuid)
         uow.verifications.add(verification=verification)
 
     with in_memory_db() as sqlite_session:
@@ -34,12 +34,12 @@ def test_rolls_back_uncommitted_work_by_default(in_memory_db):
 
 
 def test_rolls_back_on_error(in_memory_db):
-    _uuid = f'{uuid4()}'
+    uuid = f'{uuid4()}'
 
     uow = UnitOfWork(session_factory=in_memory_db)
     with pytest.raises(Exception):
         with uow:
-            verification = model.Verification(email='user@example.com', uuid=_uuid)
+            verification = model.Verification(email='user@example.com', uuid=uuid)
             uow.verifications.add(verification=verification)
             raise Exception()
 
