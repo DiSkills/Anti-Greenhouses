3bb33d6c3c5aa1a40094e247a5750a54f16bd618
_Anti_ <anti.python.fastapi@gmail.com>
2022-07-18 11:32:22 +0300

    Refactoring test_users.py

--- tests/auth/e2e/test_users.py
+++ tests/auth/e2e/test_users.py
@@ -1,18 +1,23 @@
-import uuid
 from datetime import datetime
 from typing import Literal
+from uuid import uuid4
 
 from fastapi import status
 from typing_extensions import TypeAlias
 
 from main import app
 from src.auth.domain import model
+from tests.conftest import TestData
 
 registration_data: TypeAlias = dict[Literal['username', 'email', 'password', 'confirm_password', 'uuid'], str]
 
 
 def _get_registration_data(
-    *, uuid: str, username: str = 'test', email: str = 'user@example.com', password: str = 'Admin2248!',
+    *,
+    uuid: str,
+    username: str = TestData.username.test,
+    email: str = TestData.email.user,
+    password: str = TestData.password.strong,
 ) -> registration_data:
     return {
         'username': username,
@@ -23,7 +28,13 @@ def _get_registration_data(
     }
 
 
-def _create_user(*, e2e, username: str = 'test', email: str = 'user@example.com', password: str = 'Admin2248!') -> None:
+def _create_user(
+    *,
+    e2e,
+    username: str = TestData.username.test,
+    email: str = TestData.email.user,
+    password: str = TestData.password.strong,
+) -> None:
     e2e.session.execute(
         'INSERT INTO users (username, email, password, otp_secret, otp, is_superuser, avatar, date_joined)'
         ' VALUES (:username, :email, :password, :secret, FALSE, FALSE, NULL, :date_joined)',
@@ -38,20 +49,24 @@ def _create_user(*, e2e, username: str = 'test', email: str = 'user@example.com'
     e2e.session.commit()
 
 
+def _create_verification(*, e2e, email: str = TestData.email.user) -> str:
+    uuid = f'{uuid4()}'
+    e2e.session.execute(
+        'INSERT INTO verifications (email, uuid) VALUES (:email, :uuid)', {'email': email, 'uuid': uuid},
+    )
+    e2e.session.commit()
+    return uuid
+
+
 # TODO tests for schema
 
 def test_registration_return_201_and_create_user(e2e):
     # Create verification
-    _uuid = f'{uuid.uuid4()}'
-    email = 'user@example.com'
-    e2e.session.execute(
-        'INSERT INTO verifications (email, uuid) VALUES (:email, :uuid)', {'email': email, 'uuid': _uuid},
-    )
-    e2e.session.commit()
+    uuid = _create_verification(e2e=e2e)
 
     # Database check
     rows = tuple(e2e.session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('user@example.com', _uuid),)
+    assert rows == ((TestData.email.user, uuid),)
     rows = tuple(e2e.session.execute('SELECT * FROM "users"'))
     assert rows == ()
     rows = tuple(e2e.session.execute('SELECT * FROM "user_actions"'))
@@ -60,7 +75,7 @@ def test_registration_return_201_and_create_user(e2e):
     assert rows == ()
 
     # Send request
-    response = e2e.client.post(f'{app.url_path_for("registration")}', json=_get_registration_data(uuid=_uuid))
+    response = e2e.client.post(f'{app.url_path_for("registration")}', json=_get_registration_data(uuid=uuid))
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == {'msg': 'You have been successfully registered on our website!'}
 
@@ -68,10 +83,10 @@ def test_registration_return_201_and_create_user(e2e):
     rows = tuple(e2e.session.execute('SELECT email, uuid FROM "verifications"'))
     assert rows == ()
     rows = tuple(e2e.session.execute('SELECT username FROM "users"'))
-    assert rows == (('test',),)
+    assert rows == ((TestData.username.test,),)
 
     password, = tuple(e2e.session.execute('SELECT password FROM "users"'))[0]
-    assert model.check_password_hash(password='Admin2248!', hashed_password=password) is True
+    assert model.check_password_hash(password=TestData.password.strong, hashed_password=password) is True
 
     # Check actions
     rows = tuple(e2e.session.execute('SELECT id, user_id, action_id FROM "user_actions"'))
@@ -83,44 +98,39 @@ def test_registration_return_201_and_create_user(e2e):
 def test_registration_return_400_when_user_with_this_username_exists(e2e):
     _create_user(e2e=e2e)
     rows = tuple(e2e.session.execute('SELECT username FROM "users"'))
-    assert rows == (('test',),)
+    assert rows == ((TestData.username.test,),)
 
     response = e2e.client.post(f'{app.url_path_for("registration")}', json=_get_registration_data(uuid='bad-uuid'))
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {'detail': 'User with this username exists.'}
 
     rows = tuple(e2e.session.execute('SELECT username FROM "users"'))
-    assert rows == (('test',),)
+    assert rows == ((TestData.username.test,),)
 
 
 def test_registration_return_400_when_user_with_this_email_exists(e2e):
     _create_user(e2e=e2e, username='user')
     rows = tuple(e2e.session.execute('SELECT email FROM "users"'))
-    assert rows == (('user@example.com',),)
+    assert rows == ((TestData.email.user,),)
 
     response = e2e.client.post(f'{app.url_path_for("registration")}', json=_get_registration_data(uuid='bad-uuid'))
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {'detail': 'User with this email exists.'}
 
     rows = tuple(e2e.session.execute('SELECT email FROM "users"'))
-    assert rows == (('user@example.com',),)
+    assert rows == ((TestData.email.user,),)
 
 
-def test_registration_return_400_when_invalid_verification_uuid(mocker, e2e):
+def test_registration_return_400_when_invalid_verificationuuid(mocker, e2e):
     mocker.patch('worker.send_email_task', return_value=None)
     mocker.patch('src.base.send_email.send_email', return_value=None)
 
     # Create verification
-    _uuid = f'{uuid.uuid4()}'
-    email = 'user@example.com'
-    e2e.session.execute(
-        'INSERT INTO verifications (email, uuid) VALUES (:email, :uuid)', {'email': email, 'uuid': _uuid},
-    )
-    e2e.session.commit()
+    uuid = _create_verification(e2e=e2e)
 
     # Database check
     rows = tuple(e2e.session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('user@example.com', _uuid),)
+    assert rows == ((TestData.email.user, uuid),)
     rows = tuple(e2e.session.execute('SELECT * FROM "users"'))
     assert rows == ()
 
@@ -133,7 +143,7 @@ def test_registration_return_400_when_invalid_verification_uuid(mocker, e2e):
 
     # Database check
     rows = tuple(e2e.session.execute('SELECT email, uuid FROM "verifications"'))
-    assert rows == (('user@example.com', _uuid),)
+    assert rows == ((TestData.email.user, uuid),)
     rows = tuple(e2e.session.execute('SELECT * FROM "users"'))
     assert rows == ()
 
