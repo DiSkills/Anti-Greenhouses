1977f2c9026d3599e2753588e6c72f09d155d01f
_Anti_ <anti.python.fastapi@gmail.com>
2022-07-20 15:42:06 +0300

    Added MongoDB fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,13 @@ from typing import Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from pymongo.collection import Collection
+from pymongo.database import Database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers, Session
 
 import main
-from config import metadata, start_mappers, get_session
+from config import metadata, start_mappers, get_session, mongo_client, mongo_config, MongoTables
 
 
 @pytest.fixture()
@@ -27,6 +29,24 @@ def in_memory_db():
 def sqlite_session(in_memory_db) -> Generator[Session, None, None]:
     with in_memory_db() as session:
         yield session
+
+
+@pytest.fixture()
+def mongo_session() -> Generator[Database, None, None]:
+    db = mongo_client[mongo_config.name]
+
+    verifications_table = db.create_collection(MongoTables.verifications.name)
+    verifications_table.create_index(MongoTables.verifications.uuid, unique=True)
+    verifications_table.create_index(MongoTables.verifications.email, unique=True)
+
+    yield db
+
+    mongo_client.drop_database(mongo_config.name)
+
+
+@pytest.fixture()
+def mongo_verifications(mongo_session) -> Generator[Collection, None, None]:
+    yield mongo_session[MongoTables.verifications.name]
 
 
 @dataclass
