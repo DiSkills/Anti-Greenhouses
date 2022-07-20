3791d8f4f1c7a328661105b9c8a545585e9003b6
_Anti_ <anti.python.fastapi@gmail.com>
2022-07-20 15:49:35 +0300

    Changed test_uow.py from verifications to users

--- tests/base/integration/test_uow.py
+++ tests/base/integration/test_uow.py
@@ -1,5 +1,3 @@
-from uuid import uuid4
-
 import pytest
 
 from src.auth.domain import model
@@ -7,43 +5,37 @@ from src.base.uow import UnitOfWork
 from tests.conftest import TestData
 
 
-def test_uow_can_save_a_verification(in_memory_db):
-    uuid = f'{uuid4()}'
-
+def test_uow_can_save_a_user(in_memory_db):
     uow = UnitOfWork(session_factory=in_memory_db)
     with uow:
-        verification = model.Verification(email=TestData.email.user, uuid=uuid)
-        uow.verifications.add(verification=verification)
+        user = model.User(username=TestData.username.test, email=TestData.email.user, password='hashed_password')
+        uow.users.add(user=user)
         uow.commit()
 
     with in_memory_db() as sqlite_session:
-        rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
-        assert rows == ((TestData.email.user, uuid),)
+        rows = tuple(sqlite_session.execute('SELECT username FROM "users"'))
+        assert rows == ((TestData.username.test,),)
 
 
 def test_rolls_back_uncommitted_work_by_default(in_memory_db):
-    uuid = f'{uuid4()}'
-
     uow = UnitOfWork(session_factory=in_memory_db)
     with uow:
-        verification = model.Verification(email=TestData.email.user, uuid=uuid)
-        uow.verifications.add(verification=verification)
+        user = model.User(username=TestData.username.test, email=TestData.email.user, password='hashed_password')
+        uow.users.add(user=user)
 
     with in_memory_db() as sqlite_session:
-        rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
+        rows = tuple(sqlite_session.execute('SELECT username FROM "users"'))
         assert rows == ()
 
 
 def test_rolls_back_on_error(in_memory_db):
-    uuid = f'{uuid4()}'
-
     uow = UnitOfWork(session_factory=in_memory_db)
     with pytest.raises(Exception):
         with uow:
-            verification = model.Verification(email=TestData.email.user, uuid=uuid)
-            uow.verifications.add(verification=verification)
+            user = model.User(username=TestData.username.test, email=TestData.email.user, password='hashed_password')
+            uow.users.add(user=user)
             raise Exception()
 
     with in_memory_db() as sqlite_session:
-        rows = tuple(sqlite_session.execute('SELECT email, uuid FROM "verifications"'))
+        rows = tuple(sqlite_session.execute('SELECT username FROM "users"'))
         assert rows == ()
