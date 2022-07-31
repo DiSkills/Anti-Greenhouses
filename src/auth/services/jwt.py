c6079fc9dcf9e494c86b506a4af0b8b91837b248
_Anti_ <anti.python.fastapi@gmail.com>
2022-07-31 18:02:47 +0300

    Added create_access_token()

--- src/auth/services/jwt.py
+++ src/auth/services/jwt.py
@@ -12,3 +12,13 @@ def create_token(*, data: dict[str, Any], expires_delta: Seconds = JWTConfig.def
     expire = datetime.utcnow() + timedelta(seconds=expires_delta)
     encode.update({'exp': expire})
     return jwt.encode(encode, config.get_app_settings().secret_key, algorithm=JWTConfig.algorithms[0])
+
+
+def create_access_token(*, user_id: int, uuid: str, is_superuser: bool = False) -> str:
+    data = {
+        'user_id': user_id,
+        'uuid': uuid,
+        'is_superuser': is_superuser,
+        'subject': JWTConfig.access_subject,
+    }
+    return create_token(data=data, expires_delta=JWTConfig.access)
--- tests/auth/unit/test_jwt.py
+++ tests/auth/unit/test_jwt.py
@@ -4,7 +4,7 @@ import jwt
 
 import config
 from config import JWTConfig
-from src.auth.services.jwt import create_token
+from src.auth.services.jwt import create_token, create_access_token
 
 
 def test_create_token():
@@ -15,6 +15,20 @@ def test_create_token():
         expires_delta=JWTConfig.access,
     )
     decoded = jwt.decode(access_token, config.get_app_settings().secret_key, algorithms=JWTConfig.algorithms)
+    assert decoded == {
+        'user_id': 1,
+        'is_superuser': False,
+        'uuid': uuid,
+        'subject': JWTConfig.access_subject,
+        'exp': decoded.get('exp'),
+    }
+
+
+def test_create_access_token():
+    uuid = f'{uuid4()}'
+
+    token = create_access_token(user_id=1, uuid=uuid, is_superuser=False)
+    decoded = jwt.decode(token, config.get_app_settings().secret_key, algorithms=JWTConfig.algorithms)
     assert decoded == {
         'user_id': 1,
         'is_superuser': False,
