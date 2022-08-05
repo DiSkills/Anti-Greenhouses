bff7c0d8ccff9295f47e91949946aea20c50487b
_Anti_ <anti.python.fastapi@gmail.com>
2022-08-05 13:08:17 +0300

    Added secret password validate

--- src/auth/entrypoints/routers/login.py
+++ src/auth/entrypoints/routers/login.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request, HTTPException, status, Form
+from pydantic import SecretStr
 
 from src.auth import exceptions
 from src.auth.entrypoints.schemas.users import Login
@@ -16,9 +17,9 @@ login_router = APIRouter()
     response_description='Tokens',
     response_model=Login,
 )
-async def login(request: Request, username: str = Form(...), password: str = Form(...)) -> Tokens:
+async def login(request: Request, username: str = Form(...), password: SecretStr = Form(...)) -> Tokens:
     try:
-        tokens = services.login(username=username, password=password, ip_address=request.state.ip)
+        tokens = services.login(username=username, password=password.get_secret_value(), ip_address=request.state.ip)
     except exceptions.InvalidUsernameOrPassword:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid username or password.')
 
